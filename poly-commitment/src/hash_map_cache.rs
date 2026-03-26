d86761ed220ad231c0970fa9e3c190ec9ae620ef
Richard Pringle <rpring9@gmail.com>
2026-03-26 19:20:47 -0400

    Fix unsound return reference

--- poly-commitment/src/hash_map_cache.rs
+++ poly-commitment/src/hash_map_cache.rs
@@ -1,15 +1,14 @@
-#![allow(unsafe_code)]
-
 use std::{
     cmp::Eq,
-    collections::{hash_map::Entry, HashMap},
+    collections::HashMap,
     hash::Hash,
+    ops::Deref,
     sync::{Arc, Mutex},
 };
 
 #[derive(Debug, Clone, Default)]
 pub struct HashMapCache<Key: Hash, Value> {
-    contents: Arc<Mutex<HashMap<Key, Value>>>,
+    contents: Arc<Mutex<HashMap<Key, Arc<Value>>>>,
 }
 
 impl<Key: Hash + Eq, Value> HashMapCache<Key, Value> {
@@ -21,7 +20,7 @@ impl<Key: Hash + Eq, Value> HashMapCache<Key, Value> {
     }
 
     #[must_use]
-    pub fn new_from_hashmap(hashmap: HashMap<Key, Value>) -> Self {
+    pub(crate) fn new_from_hashmap(hashmap: HashMap<Key, Arc<Value>>) -> Self {
         Self {
             contents: Arc::new(Mutex::new(hashmap)),
         }
@@ -34,7 +33,7 @@ impl<Key: Hash + Eq, Value> HashMapCache<Key, Value> {
     /// Panics if the internal mutex is poisoned.
     pub fn set(&self, key: Key, value: Value) {
         let mut hashmap = self.contents.lock().unwrap();
-        let _ = hashmap.entry(key).or_insert(value);
+        let _ = hashmap.entry(key).or_insert_with(|| Arc::new(value));
     }
 
     /// Retrieves a cached value by key, or generates and caches it using the
@@ -43,29 +42,15 @@ impl<Key: Hash + Eq, Value> HashMapCache<Key, Value> {
     /// # Panics
     ///
     /// Panics if the internal mutex is poisoned.
-    pub(crate) fn get_or_generate<'a, F: FnOnce() -> Value>(
-        &'a self,
+    #[allow(clippy::significant_drop_tightening)] // it's a flase positive, you can't drop the lock any earlier
+    pub(crate) fn get_or_generate<F: FnOnce() -> Value>(
+        &self,
         key: Key,
         generator: F,
-    ) -> &'a Value {
+    ) -> impl Deref<Target = Value> + '_ {
         let mut hashmap = self.contents.lock().unwrap();
-        let entry = (*hashmap).entry(key);
-        let inner_ptr = match entry {
-            Entry::Occupied(o) => {
-                let o_ref = o.into_mut();
-                std::ptr::from_ref(o_ref)
-            }
-            Entry::Vacant(v) => {
-                let v_ref = v.insert(generator());
-                std::ptr::from_ref(v_ref)
-            }
-        };
-        drop(hashmap);
-
-        // This is safe because we never delete entries from the cache, and the
-        // value reference must live at least at most as long as the cache
-        // value.
-        unsafe { &*inner_ptr }
+        let entry = hashmap.entry(key).or_insert_with(|| Arc::new(generator()));
+        Arc::clone(entry)
     }
 
     /// Returns `true` if the cache contains the given key.
@@ -80,7 +65,9 @@ impl<Key: Hash + Eq, Value> HashMapCache<Key, Value> {
 
 #[allow(clippy::implicit_hasher)]
 #[allow(clippy::fallible_impl_from)]
-impl<Key: Hash + Eq + Clone, Value: Clone> From<HashMapCache<Key, Value>> for HashMap<Key, Value> {
+impl<Key: Hash + Eq + Clone, Value: Clone> From<HashMapCache<Key, Value>>
+    for HashMap<Key, Arc<Value>>
+{
     fn from(cache: HashMapCache<Key, Value>) -> Self {
         cache.contents.lock().unwrap().clone()
     }
